1353bdc46ca59c8fc181a9a078c46ec89b14164c
Sphaerista <cosaleks@gmail.com>
2023-05-04 20:22:45 +0300

    redux toolkit & antd init

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Paths } from "./paths";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import "./index.css";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <Login />,
+  },
+  {
+    path: Paths.register,
+    element: <Register />,
+  },
+]);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
---
+++ client/src/paths.ts
@@ -0,0 +1,10 @@
+// all the route paths
+export const Paths = {
+  home: "/",
+  employeeAdd: "/employee/edit",
+  employeeEdit: "/employee/edit",
+  employee: "/employee",
+  status: "/status",
+  login: "/login",
+  register: "/register",
+} as const;
