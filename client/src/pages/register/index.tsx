f76dadffeda512391c4f8d22986106a3ee192984
Sphaerista <cosaleks@gmail.com>
2023-05-05 11:14:16 +0300

    register page

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
+import { CustomInput } from "../../components/custom-input";
+import { PasswordInput } from "../../components/password-input/input";
+import { CustomButton } from "../../components/custom-button";
+import { Layout } from "../../components/layout";
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Register" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput name="name" placeholder="Name" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Password" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Repeat password"
+            />
+            <CustomButton type="primary" htmlType="submit">
+              Register
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Have an account? <Link to={Paths.login}>Login</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  );
 };
