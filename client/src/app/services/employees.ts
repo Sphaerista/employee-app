fde5b374edab84ec3ce40594f8cb106a24fe0eab
Sphaerista <cosaleks@gmail.com>
2023-05-05 22:27:45 +0300

    employees slice and store

---
+++ client/src/app/services/employees.ts
@@ -0,0 +1,57 @@
+import { Employee } from "@prisma/client";
+import { api } from "./api";
+
+export const employeeApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllEmployees: builder.query<Employee[], void>({
+      query: () => ({
+        url: "/employee",
+        method: "GET",
+      }),
+    }),
+    getEmployee: builder.query<Employee, string>({
+      query: (id) => ({
+        url: `/employee/${id}`,
+        method: "GET",
+      }),
+    }),
+    editEmployee: builder.mutation<string, Employee>({
+      query: (employee) => ({
+        url: `/employee/edit/${employee.id}`,
+        method: "PUT",
+      }),
+    }),
+    removeEmployee: builder.mutation<string, string>({
+      query: (id) => ({
+        url: `/employee/remove/${id}`,
+        method: "POST",
+        body: { id },
+      }),
+    }),
+    addEmployee: builder.mutation<Employee, Employee>({
+      query: (employee) => ({
+        url: "/employee/add",
+        method: "POST",
+        body: { employee },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetAllEmployeesQuery,
+  useGetEmployeeQuery,
+  useEditEmployeeMutation,
+  useRemoveEmployeeMutation,
+  useAddEmployeeMutation,
+} = employeeApi;
+
+export const {
+  endpoints: {
+    getAllEmployees,
+    getEmployee,
+    editEmployee,
+    removeEmployee,
+    addEmployee,
+  },
+} = employeeApi;
---
+++ client/src/features/employees/employeesSlice.ts
@@ -0,0 +1,32 @@
+import { Employee } from "@prisma/client";
+import { createSlice } from "@reduxjs/toolkit";
+import { employeeApi } from "../../app/services/employees";
+import { RootState } from "../../app/store";
+
+interface InitialState {
+  employees: Employee[] | null;
+}
+
+const initialState: InitialState = {
+  employees: null,
+};
+
+const slice = createSlice({
+  name: "employees",
+  initialState,
+  reducers: {
+    logout: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      employeeApi.endpoints.getAllEmployees.matchFulfilled,
+      (state, action) => {
+        state.employees = action.payload;
+      }
+    );
+  },
+});
+
+export default slice.reducer;
+
+export const selectEmployees = (state: RootState) => state.employees;
